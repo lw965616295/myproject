13ffae423fb686ca90367dd775167539c0f51eeb
weil <1019865454@qq.com>
2021-09-27 15:01:37 +0800

    动态代理

--- reflect/src/main/java/com/weil/reflect/TestDemo.java
+++ reflect/src/main/java/com/weil/reflect/TestDemo.java
@@ -7,18 +7,22 @@ import com.weil.reflect.bean.Student;
 import com.weil.reflect.proxy.Animal;
 import com.weil.reflect.proxy.Dog;
 import com.weil.reflect.proxy.MyProxy;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.Test;
 
 import java.lang.reflect.*;
 
 
 /**
- * @ClassName Test
+ * @ClassName TestDemo
  * @Author liwei
- * @Description //TODO
+ * @Description //测试
  * @Date 20:54 2020/5/19
  * @Version 1.0.0
  **/
-public class Test {
+public class TestDemo {
     public static void main(String[] args) throws Exception{
         // 创建对象阶段
         Student student = new Student();
@@ -65,21 +69,68 @@ public class Test {
         Object invoke = toString.invoke(stu);
         System.out.println(invoke);
 
+
+
+    }
+    /**
+     * 测试动态代理
+     */
+    @Test
+    public void testProxy() throws Exception{
         // 动态代理
         Dog dog = Dog.class.newInstance();
         dog.eat();
         Animal d1 = (Animal) Proxy.newProxyInstance(dog.getClass().getClassLoader(), dog.getClass().getInterfaces(), new InvocationHandler() {
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 System.out.println("---吃饭前");
-                method.invoke(proxy, args);
+                Object invoke = method.invoke(dog, args);
                 System.out.println("---吃饭后");
-                return proxy;
+                return invoke;
             }
         });
         d1.eat();
-        // 自定义动态代理
-//        MyProxy myProxy = new MyProxy(dog);
-//        myProxy.invoke(Dog.class, "eat");
+    }
 
+    /**
+     * 测试自定义动态代理
+     */
+    @Test
+    public void testProxy1(){
+        try {
+            // 自定义动态代理
+            Dog dog = new Dog();
+            MyProxy myProxy = new MyProxy(dog);
+            myProxy.invoke(Dog.class, "eat");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 测试cglib代理
+     * 该方法可以允许被代理的类不用实现接口
+     */
+    @Test
+    public void testCglib(){
+        Enhancer enhancer = new Enhancer();
+        // 设置类加载器
+        enhancer.setClassLoader(Dog.class.getClassLoader());
+        // 设置被代理类
+        enhancer.setSuperclass(Dog.class);
+        // 设置处理
+        enhancer.setCallback(new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("吃饭前---");
+                Object invoke = methodProxy.invokeSuper(o, objects);
+                System.out.println("吃饭后---");
+                return invoke;
+            }
+        });
+        Dog dog = (Dog) enhancer.create();
+        dog.eat();
     }
 }
