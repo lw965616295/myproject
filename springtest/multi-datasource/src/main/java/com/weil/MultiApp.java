bfb78e9debe580a8ade9be87e02d7a86808a605e
weil <1019865454@qq.com>
2022-02-28 10:10:41 +0800

    多数据源

---
+++ springtest/multi-datasource/src/main/java/com/weil/MultiApp.java
@@ -0,0 +1,18 @@
+package com.weil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName MultiApp
+ * @Author weil
+ * @Description //启动类
+ * @Date 2022/2/23 9:17
+ * @Version 1.0.0
+ **/
+@SpringBootApplication
+public class MultiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiApp.class, args);
+    }
+}
